a961d63016e285bb473d540f0564648aab386238
tanjiakai <jiakai.tan@gmail.com>
2015-10-01 09:06:12 +0800

    added sort display by url.id

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,15 +3,16 @@ get '/' do
   puts "[LOG] Getting /"
   puts "[LOG] Params: #{params.inspect}"
   @shortened_url = []
-  @url = Url.all
+  @url = Url.all.order(:id)
   erb :index
 end
 
 get '/:shortened' do
   puts "[LOG] Params: #{params.inspect}"
-  url = Url.find_by(shortened_url: params[:shortened]).url
+  url = Url.find_by(shortened_url: params[:shortened])
   url.clicks += 1
-  redirect to "#{url}"
+  url.save
+  redirect to "#{url.url}"
 end
 
 post '/urls' do
